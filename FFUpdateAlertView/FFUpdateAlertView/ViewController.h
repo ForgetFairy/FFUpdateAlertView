e260d5459258b058306a49763cd48a82d5ec79c6
ForgetFairy <1983200492@qq.com>
2018-06-01 15:04:18 +0800

    first commit

---
+++ FFUpdateAlertView/FFUpdateAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFUpdateAlertView
+//
+//  Created by ForgetFairy on 2018/3/7.
+//  Copyright © 2018年 ForgetFairy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFUpdateAlertView/FFUpdateAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFUpdateAlertView
+//
+//  Created by ForgetFairy on 2018/3/7.
+//  Copyright © 2018年 ForgetFairy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
